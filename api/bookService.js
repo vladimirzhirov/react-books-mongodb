30b9d6253f39a18830809d249c9a7706defa79ec
vladimirzhirov <vladimir.zhirov@accenture.com>
2019-03-28 10:50:38 +0300

    ref

--- api/bookService.js
+++ api/bookService.js
@@ -1,10 +1,10 @@
-import {isObject} from "lodash";
+const lodash = require("lodash");
 const books = require("./model");
 
 const create = (file, req, res, next) => {
   let book = new books(req.body);
 
-  if (file) {
+  if (lodash.isObject(file)) {
     book.image = { url: file.url, name: file.name };
   }
 
@@ -23,7 +23,7 @@ const update = (file, book, req, res, next) => {
   book.description = req.body.description;
   book.isbn = req.body.isbn;
 
-  if (file) {
+  if (lodash.isObject(file)) {
     book.image = { url: file.url, name: file.name };
   }
 
